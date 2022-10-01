2fc74e0f85be5c429127078f05a6e925358a5055
KrasimirKostadinov <krasimir.kostadinov@modis.com>
2022-10-01 19:59:45 +0300

    updated prettier

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { AppModule } from './modules/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.useGlobalPipes(new ValidationPipe({}));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   await app.listen(9999);
 }
 bootstrap();
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { User } from './interfaces/user.interface';
 
 @Injectable()
 export class UsersService {
@@ -9,6 +8,7 @@ export class UsersService {
   }
 
   createUser(user: CreateUserDto) {
+    console.log(user);
     return `username: ${user.username}`;
   }
 
